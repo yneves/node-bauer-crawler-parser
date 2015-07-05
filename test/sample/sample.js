ae328b543fd4d68e27b23add8ed913fb8c32e047
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-07-05 17:03:29 -0300

    init

---
+++ test/sample/sample.js
@@ -0,0 +1,42 @@
+// - -------------------------------------------------------------------- - //
+
+"use strict";
+
+var fs = require("fs");
+var assert = require("assert");
+var Crawler = require("bauer-crawler");
+
+var crawler = new Crawler();
+
+crawler.require(__dirname + "/../../");
+
+crawler.ready(function() {
+  
+  this.promise()
+    .scrape(__dirname + "/sample.html",{
+      "head > title": {
+        title: "text",
+      },
+      "[id]": {
+        ids: {
+          "a[href][title]": {
+            url: "attr:href",
+            title: "attr:title"
+          }
+        }
+      }
+    })
+    .then(function(file) {
+      var output = JSON.parse(fs.readFileSync(file).toString());
+      var compare = JSON.parse(fs.readFileSync(__dirname + "/sample.json").toString());
+      assert.deepEqual(output,compare);
+      fs.unlinkSync(file);
+    })
+    .exit();
+  
+  
+});
+
+crawler.start();
+
+// - -------------------------------------------------------------------- - //
